7d92b43e35121e016f1da8fad24a57a4bcb2c3b3
Mehul773 <chovatiyamehul2003@gmail.com>
2024-02-23 13:01:49 +0530

    Add comments and improve code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,55 @@
-// App.js
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import { saveAs } from "file-saver";
-import loadingGif from "../assets/loading.gif"; // Import your loading GIF
+import loadingGif from "../assets/loading.gif";
 
+// Base URL for the backend server
 const BASE_URL = "http://localhost:5000";
 
 function App() {
+  // State variables to manage URL input, loading state, and error messages
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+  // Function to handle form submission
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setError("");
+    event.preventDefault(); // Prevent default form submission behavior
+    setLoading(true); // Set loading state to true
+    setError(""); // Clear any previous error messages
 
     try {
+      // Send a POST request to the backend server to convert the URL to PDF
       const response = await axios.post(
         `${BASE_URL}/convert-to-pdf-puppeteer`,
         { url },
-        { responseType: "arraybuffer" } // tell axios to respond with an array buffer
+        { responseType: "arraybuffer" } // Tell axios to respond with an array buffer
       );
 
-      // Convert the array buffer to a blob
+      // Convert the array buffer received from the server to a Blob
       const pdfBlob = new Blob([response.data], { type: "application/pdf" });
 
-      // Use file-saver to save the file
+      // Use file-saver to save the PDF file locally
       saveAs(pdfBlob, "output.pdf");
+
+      // Reset loading state and URL input after successful conversion
       setLoading(false);
       setUrl("");
     } catch (error) {
+      // Handle errors if any occur during the conversion process
       console.error("Error converting to PDF:", error);
       setError("Error converting to PDF. Please try again later.");
-      setLoading(false);
+      setLoading(false); // Reset loading state
     }
   };
 
+  // Render the component
   return (
     <div className="outer-container">
+      <h1>Convert web page to pdf</h1>
       <div className="container">
+        {/* Form for entering the URL and submitting the conversion request */}
         <form onSubmit={handleSubmit} className="input-form">
           <input
             type="url"
@@ -49,14 +58,17 @@ function App() {
             placeholder="Enter URL"
             required
           />
+          {/* Button to initiate the conversion process */}
           <button type="submit" disabled={loading} className="btn">
+            {/* Display loading animation if loading, otherwise display button text */}
             {loading ? (
-              <img src={loadingGif} alt="Loading..." /> // Render loading GIF if loading
+              <img src={loadingGif} alt="Loading..." />
             ) : (
               "Convert to PDF"
             )}
           </button>
         </form>
+        {/* Display error message if conversion fails */}
         {error && <div>{error}</div>}
       </div>
     </div>
